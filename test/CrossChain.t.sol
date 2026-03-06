7263694ace666c2a908442e350c6f94af7df83b4
refaldy <refaldybanggana@gmail.com>
2026-03-07 05:18:28 +0700

    deploy token test

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -12,9 +12,17 @@ import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
 contract CrossChainTest is Test {
+    address owner = makeAddr("owner");
+
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
+
     CCIPLocalSimulatorFork ccipSimulatorFork;
+
+    RebaseToken sepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    Vault vault;
 
     function setUp() public {
         sepoliaFork = vm.createSelectFork("sepolia-eth");
@@ -22,5 +30,17 @@ contract CrossChainTest is Test {
 
         ccipSimulatorFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipSimulatorFork));
+
+        // 1. Deploy and configure on Sepolia
+        vm.startPrank(owner);
+        sepoliaToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(sepoliaToken)));
+        vm.stopPrank();
+
+        // 2. Deploy and configure on Arbitrum Sepolia
+        vm.selectFork(arbSepoliaFork);
+        vm.startPrank(owner);
+        arbSepoliaToken = new RebaseToken();
+        vm.stopPrank();
     }
 }
